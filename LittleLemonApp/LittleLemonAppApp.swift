1a6497aaa1c2c9936cb3518a3b69dc3638bbc539
Najoua MAHI <najoua.mahi@es.nestle.com>
2023-02-25 21:05:42 -0600

    Initial Commit

---
+++ LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Najoua Mahi on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
